e251cf2835da3465f7c190691c4b57ba5eb78ab3
= <=>
2022-12-16 21:19:12 +0300

    Initial Commit

---
+++ Hacker-Rank-Problems-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Hacker-Rank-Problems-C
+//
+//  Created by Özgür Kamalı on 16.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
